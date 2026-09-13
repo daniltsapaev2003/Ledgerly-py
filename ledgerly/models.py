fd572648589d72407043a8ce031c33eefc392def
Danil <DanilTs@MacBook-Pro-Alexey.local>
2026-09-13 17:45:53 +0300

    Создана страница Логина/регистрации + функции проверки пароля и создание пользователя

---
+++ ledgerly/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class User(models.Model):
+    class Meta: db_table = "users"
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=128)
---
+++ ledgerly/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from .models import User
+from django.db import IntegrityError
+from django.contrib.auth.hashers import make_password 
+from django.contrib.auth.hashers import check_password
+
+
+def registration(request):
+    if request.method == "POST":
+        try:
+                User.objects.create(
+                first_name=request.POST["first_name"],
+                last_name=request.POST["last_name"],
+                email=request.POST["email"],
+                phone_number=request.POST["phone_number"],
+                password=make_password(request.POST["password"])
+                )
+                return redirect("dashboard")
+
+        except IntegrityError:
+            return render(
+                request,
+                "Loginpage.html",
+                {"user_exists": True}
+            )
+
+    return 0
+
+def CheckUser(request):
+    user_data = User.objects.filter(email = request.POST["email"])
+    password = request.POST["password"]
+    if user_data.exists():
+         if check_password(password,user_data.first().password):
+              return redirect("dashboard")
+         
+    
+    return render(
+                    request,
+                    "Loginpage.html",
+                    {"wrong_email_password": True}
+                )   
+   
